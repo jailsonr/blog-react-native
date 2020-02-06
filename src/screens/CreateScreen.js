93853d566ac61c85979a20f1432c81b86d22dc15
jailsonvr <jailsonvr@gmail.com>
2020-02-05 23:55:16 -0300

    added callback function

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -20,7 +20,9 @@ const CreateScreen = ({ navigation }) => {
         onChangeText={text => setContent(text)} />
         <Button 
             title="Add Blog Post"
-            onPress={() => addBlogPost(title, content)}
+            onPress={() => addBlogPost(title, content, () => {
+                navigation.navigate('Index');
+            })}
         />
     </View>
 };
